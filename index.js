a483943fa8e15ab33dcb8f225a4d490a4d31c743
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-04-16 22:47:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
-app.use("/hotels", hotelsRoute);
+app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
